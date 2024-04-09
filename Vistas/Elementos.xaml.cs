610821f7d1aec9c4cc2813842c3e3619135464e5
master <master@SMLAB304PC02_24>
2024-04-08 21:50:22 -0500

    aumentar detalle en elementos picket y date picker

--- Vistas/Elementos.xaml.cs
+++ Vistas/Elementos.xaml.cs
@@ -1,3 +1,5 @@
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 namespace gguachaminS2.Vistas;
 
 public partial class Elementos : ContentPage
@@ -6,4 +8,21 @@ public partial class Elementos : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void btnPicker_Clicked(object sender, EventArgs e)
+    {
+        if(pkCiudades.SelectedIndex == -1) {
+            DisplayAlert("Alerta", "Seleccione un país!", "Cerrar");
+        }
+        else {
+            string dato = pkCiudades.Items[pkCiudades.SelectedIndex].ToString();
+            lblDatoPicker.Text = "La ciudad seleccionado es " + dato;
+        }
+    }
+
+    private void btnFechas_Clicked(object sender, EventArgs e)
+    {
+        string fecha = dpFecha.Date.ToString();
+        lblDatoPicker.Text= "La fecha seleccionada es " + fecha;
+    }
 }
